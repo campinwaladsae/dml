7c2ee9495f58deba5c5ef8ecb8593a83eae2f94b
campinwaladsae <104126445+campinwaladsae@users.noreply.github.com>
2023-12-07 16:11:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import matplotlib.pyplot as plt
-import streamlit as st
-from datetime import date
-import pandas as pd
-from PIL import Image
-from streamlit_shiny import run_shiny_app
 from flask import Flask, request, render_template
 import pickle
 
